86e1bf0f175a9667a833ff374a3853e5a290f498
Eijgermans, P.B.J. (Peter) <p.eijgermans11@gmail.com>
2020-09-27 16:56:47 +0200

    changed component name from app.comp to weather.comp

--- Exercises Angular10/examples/C08-services-live-weather/src/main.ts
+++ Exercises Angular10/examples/C08-services-live-weather/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { WeatherModule } from './app/weather.module';
+import { WeatherModule } from './weather/weather.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
